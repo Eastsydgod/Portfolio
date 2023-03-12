48fda4bc14ff1a4c204f61c571d1ffdc4dd549f5
Jemienachukwu <jemieofoegbu@gmail.com>
2023-03-12 09:14:33 +0100

    update

--- src/About/about.js
+++ src/About/about.js
@@ -1,18 +1,15 @@
 import { Main } from "./aboutstyle";
-import { motion } from "framer-motion";
 
 function About() {
   return (
     <Main id="About">
       <div className="container">
-        <motion.div animate={{ x: 100 }}>
-          <p>
-            👋Hi, I am Ofoegbu Jemienachukwu Jeffery, a self taught frontend
-            Software Engineer with 2+ years experience working on web
-            applications using Javascript and react. I build user friendly
-            applications and observe clean code practices while building them
-          </p>
-        </motion.div>
+        <p>
+          👋Hi, I am Ofoegbu Jemienachukwu Jeffery, a self taught frontend
+          Software Engineer with 2+ years experience working on web applications
+          using Javascript and react. I build user friendly applications and
+          observe clean code practices while building them
+        </p>
         <a href="https://docs.google.com/document/d/1mtHza1j34uL7jsPPLae6cK0kcG_M4AOBflXvnIoykwY/edit?usp=sharing">
           <button>View My Resume</button>
         </a>
